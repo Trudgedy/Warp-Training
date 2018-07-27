088339287cd0cfea4402a69cb8cd292ab74300eb
Worc <trudgedy@gmail.com>
2018-07-27 15:01:33 +0200

    Revert "Validation Done"
    This reverts commit 5abc1a419cae09497258883ca0f11de28ef333cd.

--- Library/Data/Models/Common/Person.cs
+++ Library/Data/Models/Common/Person.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +13,9 @@ namespace Library.Data.Models.Common
 		}
 
 		public int PersonId { get; set; }
-		[Required]
+
 		public String Name { get; set; }
-		[Required]
+
 		public String Email { get; set; }
 
 		#region Navigational Properties
